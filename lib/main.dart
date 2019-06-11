fa06763cccdfeb012fba26920014a6e9d4647567
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-06-11 11:15:31 -0500

    Add themes Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(SnackBarDemo());
@@ -5,6 +6,7 @@ void main() => runApp(SnackBarDemo());
 class SnackBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return MaterialApp(
       title: 'SnackBar Demo',
       theme: ThemeData.dark(),
@@ -13,11 +15,37 @@ class SnackBarDemo extends StatelessWidget {
           title: Text('SnackBar Demo'),
         ),
         body: SnackBarPage(),
+=======
+    final appName = 'Custom Themes';
+
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData(
+        // Define the default Brightness and Colors
+        brightness: Brightness.dark,
+        primaryColor: Colors.amber[800],
+        accentColor: Colors.amber[600],
+
+        // Define the default Font Family
+        fontFamily: 'Montserrat',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: MyHomePage(
+        title: appName,
+>>>>>>> Stashed changes
       ),
     );
   }
 }
 
+<<<<<<< Updated upstream
 class SnackBarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -46,6 +74,37 @@ class SnackBarPage extends StatelessWidget {
         child: Text(
           'Show SnackBar',
           style: TextStyle(color: Colors.black),
+=======
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Text with a background color',
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme:
+              Theme.of(context).colorScheme.copyWith(secondary: Colors.yellow),
+        ),
+        child: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+          backgroundColor: Color.fromRGBO(240, 230, 210, 50),
+>>>>>>> Stashed changes
         ),
       ),
     );
